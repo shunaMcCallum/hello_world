871f883ae0651b667c6c3c6d5d07104329e2035b
shunaMcCallum <shuna.mccallum@gmail.com>
2022-07-01 17:43:36 +0100

    fixed client render problem

--- client/src/components/CountryList.js
+++ client/src/components/CountryList.js
@@ -2,18 +2,19 @@ import React from 'react'
 import ListItem from './ListItem'
 
 
-const CountryList = ({countries, onCountryClick}) => {
+const CountryList = ({ countries, onCountryClick }) => {
 
-    const countriesItems = countries.map((country,index) => {
-        return <ListItem country={country} key={index} onCountryClick={onCountryClick}/>
-    })
+    // const countriesItems = countries.map((country,index) => {
+    //     return <ListItem country={country} key={index} onCountryClick={onCountryClick}/>
+    // })
 
     return (
-        <div>
-            <ul>
-                {countriesItems}
-            </ul>
-        </div>
+        // <div>
+        //     <ul>
+        //         {countriesItems}
+        //     </ul>
+        // </div>
+        <ListItem />
     )
 }
 
