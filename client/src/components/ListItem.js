f8aac84aa3bf189f36c441b930dd0c7f5525d948
shunaMcCallum <shuna.mccallum@gmail.com>
2022-07-02 20:35:02 +0100

    added logic to display which countries have or have not been studied, and show buttons to add or remove countries from studied list depending on whether the country has been studied

--- client/src/components/ListItem.js
+++ client/src/components/ListItem.js
@@ -2,29 +2,50 @@ import React from 'react';
 
 const ListItem = ({ country, onCountryClick, handleCountryStudied, handleRemoveCountryStudied, selectedUser }) => {
 
-    // function for handling what happens when list item is clicked
-    // this will run the onCountry Click function, which sets the selectedCountry state in CountriesContainer
-    const handleCountryClick = () => {
-        onCountryClick(country);
+  // function for handling what happens when list item is clicked
+  // this will run the onCountry Click function, which sets the selectedCountry state in CountriesContainer
+  const handleCountryClick = () => {
+    onCountryClick(country);
+  }
+
+  const handleStudiedClick = () => {
+    handleCountryStudied(country)
+  }
+
+  const handleRemoveStudiedClick = () => {
+    handleRemoveCountryStudied(country)
+  }
+
+  const checkIfCountryStudied = () => {
+    for (let item of selectedUser.countries_studied) {
+      if (item.name.common !== country.name.common) {
+        return false
+      } else {
+        return true
+      }
     }
-
-    const handleStudiedClick = () => {
-      handleCountryStudied(country)
-    }
-
-    const handleRemoveStudiedClick = () => {
-      handleRemoveCountryStudied(country)
-    }
+  }
 
 
-    return (
+  return (
+
+    <>
+      {checkIfCountryStudied() ?
         <>
-            <img src={country.flags.png} alt={country.name.common} onClick={handleCountryClick} />
-            <h2 onClick={handleCountryClick}><b>{country.name.common}</b></h2>
-            <button onClick={handleStudiedClick}>I've Studied this Country</button>
-            <button onClick={handleRemoveStudiedClick}>Remove from my Studied List</button>
+          <img src={country.flags.png} alt={country.name.common} onClick={handleCountryClick} />
+          <h1 onClick={handleCountryClick}>{country.name.common}</h1>
+          <p>STUDIED</p>
+          <button onClick={handleRemoveStudiedClick}>Remove from my Studied List</button>
+        </> :
+
+        <>
+          <img src={country.flags.png} alt={country.name.common} onClick={handleCountryClick} />
+          <h3 onClick={handleCountryClick}>{country.name.common}</h3>
+          <button onClick={handleStudiedClick}>Add to my Studied List</button>
         </>
-    )
+      }
+    </>
+  )
 }
 
 export default ListItem;
