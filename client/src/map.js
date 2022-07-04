40aadb68300ffb3e2b423fd8d1bc4b5f279dfe32
CleoBrown <gillmoynagh@hotmail.co.uk>
2022-07-04 21:58:47 +0100

    completed map minus marker

--- client/src/map.js
+++ client/src/map.js
@@ -4,21 +4,27 @@ import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet';
 
 
 const map = () => {
+
+
+
     return (
         <>
-            <h1>I'm a map</h1>
-            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={SVGComponentTransferFunctionElement}>
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                <Marker position={[51.505, -0.09]}>
-                    <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                </Marker>
-            </MapContainer>
-
+            <div >
+                {/* <h1>I'm a map</h1> */}
+                <MapContainer className="map" center={[51.505, -0.09]} zoom={5} scrollWheelZoom={true}>
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker position={[51.505, -0.09]}>
+                        <Popup className="popup">
+                            <p>
+                                London Bridge! <br /> United Kingdom
+                            </p>
+                        </Popup>
+                    </Marker>
+                </MapContainer>
+            </div>
 
         </>
     )
