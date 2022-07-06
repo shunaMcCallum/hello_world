19989b57b9f2fc03baa6046acf64d0149ab7b10f
shunaMcCallum <shuna.mccallum@gmail.com>
2022-07-06 11:46:02 +0100

    final shadow fixes

--- client/src/components/PaginationDetail.js
+++ client/src/components/PaginationDetail.js
@@ -24,7 +24,7 @@ function PaginationDetail({ country, currentPage }) {
                 <p>It has a <b>population</b> of {country.population.toLocaleString()} people.</p>
                 <p>The <b>capital city</b> is {country.capital}.</p>
                 <p>{country.name.common} is located in the <b>continent</b> of {country.continent}.</p>
-                <img className="pagination-image" src={country.name.img} />
+                <img className="pagination-image" id="map-picture" src={country.name.img} />
             </div>
         )
     };
@@ -64,10 +64,20 @@ function PaginationDetail({ country, currentPage }) {
             <div className="pagination-detail">
                 <h2>Landmarks</h2>
                 <p>{country.name.common}'s most famous landmarks include:</p>
-                <p>{country.landmarks[0].name}</p>
+                
+                <div className="picture">
                 <img className="pagination-image" src={country.landmarks[0].img} />
-                <p>{country.landmarks[1].name}</p>
+                <div className="image-container">
+                <p>{country.landmarks[0].name}</p>
+                    </div>
+                </div>
+
+                <div className="picture" id="picture">
                 <img className="pagination-image" src={country.landmarks[1].img} />
+                <div className="image-container">
+                <p>{country.landmarks[1].name}</p>
+                </div>
+            </div >
             </div>
         )
     };
@@ -78,7 +88,7 @@ function PaginationDetail({ country, currentPage }) {
                 <h2>Fun Fact</h2>
                 <h4>{country.funFact.header}</h4>
                 <p>{country.funFact.fact}</p>
-                <img className="pagination-image" src={country.funFact.img} />
+                <img className="pagination-image"id="map-picture" src={country.funFact.img} />
             </div>
         )
     };
@@ -108,7 +118,7 @@ function PaginationDetail({ country, currentPage }) {
         <div>
             {/* the seleced country's name is displayed and the required data on that country is rendered using getPaginatedData */}
             <h1>{country.name.common}</h1>
-            <img className="flag-image" src={country.flags.png} alt={country.name.common} />
+            <img className="flag-image" id="flag-picture" src={country.flags.png} alt={country.name.common} />
             {getPaginatedData()}
         </div>
     );
