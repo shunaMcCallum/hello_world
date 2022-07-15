c183f606ff3ee0f97ba574ecfbbc4bca6e4a91d1
shunaMcCallum <shuna.mccallum@gmail.com>
2022-07-15 11:43:51 +0100

    map changed to a hover one without markers

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -4,31 +4,43 @@ import { MapContainer, TileLayer, useMap, Marker, Popup, Tooltip, CircleMarker }
 import markerIconPng from "leaflet/dist/images/marker-icon.png";
 // import markerIconPng from "https://www.google.com/search?q=emoji+smilie+png&tbm=isch&ved=2ahUKEwj9rfH_u-H4AhWLw4UKHaUAAwUQ2-cCegQIABAA&oq=emoji+smilie+png&gs_lcp=CgNpbWcQAzIGCAAQHhAIOgQIIxAnOgcIABCxAxBDOgQIABBDOgsIABCABBCxAxCDAToKCAAQsQMQgwEQQzoICAAQgAQQsQM6BQgAEIAEOgQIABAeOgYIABAKEBhQ1QRY4h5g2SFoAHAAeACAAUOIAcwFkgECMTKYAQCgAQGqAQtnd3Mtd2l6LWltZ8ABAQ&sclient=img&ei=2AjEYv3hKYuHlwSlgYwo&bih=734&biw=1440#imgrc=-pi95dUME9mAqM";
 import { Icon } from "leaflet";
-
+import { Chart } from 'react-google-charts';
 import NavBar from "../components/NavBar";
 
 
 const Map = ({ countries }) => {
 
-    const markers = countries.map((country) => {
-        return (
-            <Marker position={country.latlng} icon={new Icon({ iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41] })}>
-                <Popup className="popup">
-                    <p>
-                        {country.name.common}
-                        <p>Population:</p>
-                        {country.population.toLocaleString()}
-                    </p>
-                </Popup>
+    // const markers = countries.map((country) => {
+    //     return (
+    //         <Marker position={country.latlng} icon={new Icon({ iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41] })}>
+    //             <Popup className="popup">
+    //                 <p>
+    //                     {country.name.common}
+    //                     <p>Population:</p>
+    //                     {country.population.toLocaleString()}
+    //                 </p>
+    //             </Popup>
 
-            </Marker>
-        )
+    //         </Marker>
+    //     )
+    // })
+
+    const data2 = countries.map((country) => {
+      return [country.name.common, country.population]
     })
+
+    const data4 = [["Country", "Population"], ...data2];
+
+    const options = {
+        colorAxis: { colors: ["#94bf7f", "#194c00"] },
+        backgroundColor: "81d4fa",
+        datalessRegionColor: "white",
+    }
 
 
     return (
         <>
-            <div>
+            {/* <div>
                 <NavBar />
             </div>
             <div >
@@ -44,7 +56,30 @@ const Map = ({ countries }) => {
 
 
                 </MapContainer>
+            </div> */}
+
+            <div>
+                <Chart
+                    chartEvents={[
+                        {
+                            eventName: "select",
+                            callback: ({ chartWrapper }) => {
+                                const chart = chartWrapper.getChart();
+                                const selection = chart.getSelection();
+                                if (selection.length === 0) return;
+                                const region = data4[selection[0].row + 1];
+                                console.log("Selected : " + region);
+                            },
+                        },
+                    ]}
+                    chartType="GeoChart"
+                    width="100%"
+                    height="400px"
+                    data={data4}
+                    options = {options}
+                />
             </div>
+
         </>
     )
 }
